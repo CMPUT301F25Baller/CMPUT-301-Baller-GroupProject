96a7753abc80e96be3e9be8ed8ad8ef64f277744
t026 <tuhintanishq@gmail.com>
2025-11-06 16:11:58 -0700

    feat: updated both views and implemented listeners for buttons

--- code/app/src/main/java/com/example/ballerevents/EventEntrantsActivity.java
+++ code/app/src/main/java/com/example/ballerevents/EventEntrantsActivity.java
@@ -1,4 +1,52 @@
 package com.example.ballerevents;
 
-public class EventEntrantsActivity {
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.chip.Chip;
+
+public class EventEntrantsActivity extends AppCompatActivity {
+    private Chip chipFilter;
+    private MaterialButton inviteButton;
+    private ImageButton backButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.organizer_event_entrants);
+
+        chipFilter = findViewById(R.id.chipFilter);
+        setupChipFilter();
+
+        inviteButton = findViewById(R.id.inviteButton);
+        inviteButton.setOnClickListener(v -> {
+            // Empty for now, will add functionality later
+        });
+
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> {
+            // Empty for now, will add functionality later
+        });
+    }
+
+    private void setupChipFilter(){
+        chipFilter.setOnClickListener(v -> {
+            PopupMenu menu = new PopupMenu(EventEntrantsActivity.this, chipFilter);
+            menu.getMenu().add("Enrolled");
+            menu.getMenu().add("Invited");
+            menu.getMenu().add("Waitlist");
+
+            menu.setOnMenuItemClickListener(item -> {
+                chipFilter.setText(item.getTitle());
+                return true;
+            });
+
+            menu.show();
+        });
+    }
 }
--- code/app/src/main/java/com/example/ballerevents/EventFormActivity.java
+++ code/app/src/main/java/com/example/ballerevents/EventFormActivity.java
@@ -1,4 +1,28 @@
 package com.example.ballerevents;
 
-public class EventFormActivity {
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventFormActivity extends AppCompatActivity {
+    private ImageButton backButton;
+    private ImageButton favButton;
+    private ImageButton editTitleButton;
+    private ImageView calendar;
+    private ImageButton editCalendarButton;
+    private ImageView location;
+    private ImageButton editLocationButton;
+
+    private ImageButton editRequirementsButton;
+    private ImageButton editAboutButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.organizer_event_form);
+    }
 }
