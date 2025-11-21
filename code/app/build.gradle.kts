bc9802f5bab6a9b98d0f2c223ed2fbe091635868
Cypest <tafeturn@gmail.com>
2025-11-20 18:37:41 -0700

    feat: generated javadoc in html format

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.dokka.Platform
+
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
+    id("org.jetbrains.dokka")
 }
 
 android {
@@ -63,4 +67,19 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.test.espresso:espresso-contrib:3.6.1")
+}
+
+tasks.withType<DokkaTask>().configureEach {
+    outputDirectory.set(layout.buildDirectory.dir("dokka/html"))
+
+    dokkaSourceSets {
+        create("androidMain") {
+            displayName.set("app")
+            platform.set(Platform.jvm)
+
+            sourceRoots.from(file("src/main/java"))
+
+            skipEmptyPackages.set(false)
+        }
+    }
 }
